5358a2f35a18a622e571c4d445d7d1d48ae38c16
Ifeoluwah <spencerife.21@gmail.com>
2025-11-19 19:11:27 +0100

    chore: resolve conflict

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Search, ShoppingCart, Bell, Menu, X } from "lucide-react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import Link from "next/link";
 
